bc12527eb0b97e6829d8673d02e45f3b1c96d73e
Jihee Kim <jiheekim93@gmail.com>
2022-03-28 15:28:32 -0400

    session changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require ('mongoose');
 const session = require('express-session')
-
+const MongoStore = require('connect-mongo')
 const cors = require('cors')
 const app = express ();
 require('dotenv').config()
