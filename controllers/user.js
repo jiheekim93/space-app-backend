2869bc480e2ad3bf28bf10cdb53346590a244423
Jihee Kim <jiheekim93@gmail.com>
2022-03-28 23:13:57 -0400

    da fuck

--- controllers/user.js
+++ controllers/user.js
@@ -3,48 +3,10 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user.js')
 
-
-router.post('/createaccount', (req, res) => {
-  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-  User.create(req.body, (err, createdUser) => {
-    if(err){
-      res.json('User already exists')
-    } else {
-      console.log('user is created', createdUser);
-      res.json(createdUser)
-    }
-  })
+const userRoutes = express.Router();
+userRoutes.post("", (req, res) => {
+  res.send(req.body);
 });
 
-router.put('/login', (req, res) => {
-  console.log(req.body);
-  User.findOne({username: req.body.username}, (err, foundUser) => {
-    if(err) {
-      res.json('Oops, there was an error. Please try again')
-    } else {
-      if(!foundUser){
-        res.json('Username and password do not match. Please try again.')
-      } else if(bcrypt.compareSync(req.body.password, foundUser.password)) {
-        res.json({username: foundUser.username})
-      } else {
-        res.json('Username and password do not match. Please try again.')
-      }
-    }
-  })
-});
-
-
-router.get('/', (req,res) => {
-  User.find({}, (err, foundUsers) => {
-    res.json(foundUsers)
-  })
-})
-
-router.get('/findOne/:username', (req,res) => {
-  User.findOne({username:req.params.username}, (err, foundUser) => {
-    res.json(foundUser)
-  })
-  // res.send('okay')
-})
 
 module.exports = router
